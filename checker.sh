d102ac86a9d8e5ad9c4831cf67dd2b6b63a0c57e
hesam-init <hesam.init@gmail.com>
2025-03-06 15:28:17 +0330

    fix: build

--- checker.sh
+++ checker.sh
@@ -30,48 +30,39 @@ log() {
 
 fetch_version() {
 	repo=$1
-	# latest_tag=$(curl -s "https://api.github.com/repos/hyprwm/$repo/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
 	latest_tag=$(curl -s "https://github.com/hyprwm/$repo/releases" | grep -Po 'href="[^"]*/releases/tag/\K[^"]+' | head -n 1)
 
-	echo $latest_tag
+	echo "$latest_tag"
 }
 
 main() {
 	command -v curl >/dev/null || {
 		log error "Curl not found, please install. Exiting..." >&2
-
 		exit 1
 	}
 
-	for package_dir in "$template_dir"/aquamarine/ "$template_dir"/hypr*/ "$template_dir"/xdg-desktop-portal-hyprland/; do
+	# Find only Hyprland-related folders, excluding *-devel
+	for package_dir in $(find "$template_dir" -mindepth 1 -maxdepth 1 -type d -name 'hypr*' ! -name '*-devel'); do
 		pkgname=$(basename "$package_dir")
-
-		if [[ "$pkgname" == "hyprland-devel" ]]; then
-			continue
-		fi
-
-		template_file="${package_dir}template"
+		template_file="${package_dir}/template"
 
 		if [[ ! -f "$template_file" ]]; then
-			echo "Template file not found for $pkgname"
-
+			log warning "Template file not found for $pkgname"
 			continue
 		fi
 
-		current_version="v$(grep -Po '^version=\K[0-9.]+(?=$)' "$template_file")"
-
+		current_version="v$(grep -Po '^version=\K[0-9.]+' "$template_file")"
 		latest_version=$(fetch_version "$pkgname")
 
 		echo "$pkgname ===> $current_version"
 
 		if [[ -z "$latest_version" ]]; then
-			log error "$pkgname ===> Failed to fetch the latest version from GitHub\n"
-
+			log error "$pkgname ===> Failed to fetch the latest version from GitHub"
 			continue
 		elif [[ "$current_version" == "$latest_version" ]]; then
-			log success "${GREEN}$pkgname is up-to-date (Version: $current_version)\n"
+			log success "$pkgname is up-to-date (Version: $current_version)"
 		else
-			log info "$pkgname: $current_version, but latest version is $latest_version\n"
+			log info "$pkgname: $current_version, but latest version is $latest_version"
 		fi
 	done
 }
